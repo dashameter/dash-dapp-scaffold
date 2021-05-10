8af74b3705f6959d945613f2530b2330274db30f
dashameter <dashameter@gmail.com>
2021-05-10 15:10:35 +0700

    feat: implement onboard task5

---
+++ pages/magic.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>Sending invite code "{{ inviteCode }}" to DashPay</div>
+</template>
+
+<script>
+import LocalMessageDuplexStream from 'post-message-stream'
+
+const pageStream = new LocalMessageDuplexStream({
+  name: 'DashPay:Dapp',
+  target: 'DashPay:content',
+})
+
+export default {
+  data() {
+    return { inviteCode: '' }
+  },
+  created() {
+    this.inviteCode = localStorage.getItem('inviteCode')
+
+    console.log('this.inviteCode :>> ', this.inviteCode)
+
+    pageStream.write({ name: 'inviteCode', payload: this.inviteCode })
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/onboard.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card>
+    <v-card-title
+      ><v-img max-height="40px" max-width="40px" src="icons/150.png" />
+      DashPay</v-card-title
+    >
+    <v-card-text class="text-center"
+      >Your invite code is:<strong>
+        {{ inviteCode || 'N/A' }}</strong
+      ></v-card-text
+    >
+    <v-card-text class="text-center">
+      <v-btn href="chrome://extensions" color="primary"
+        >Click here to install DashPay</v-btn
+      >
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return { inviteCode: '' }
+  },
+  created() {
+    console.log('this.$route :>> ', this.$route)
+    this.inviteCode = this.$route.query.invite
+    localStorage.setItem('inviteCode', this.inviteCode)
+  },
+}
+</script>
+
+<style></style>
